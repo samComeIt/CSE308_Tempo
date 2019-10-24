3e264a896bed28b315dbe2233ad801bd95b69e64
samComeIt <40321847+samComeIt@users.noreply.github.com>
2019-10-24 21:58:53 +0900

    Revert "Added login page"
    This reverts commit aa714a4277d01e8f9f15ec9b3b36f7161baf32df.

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('auth/login');
+    return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
