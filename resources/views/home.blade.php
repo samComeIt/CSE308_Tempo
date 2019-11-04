f7f042d14c685f0274159c85af710f5c152d0108
khpark228 <kyuhee.park@stonybrook.edu>
2019-11-05 03:21:30 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+
+<section id="main" class="wrapper">
+    <div class="inner">
+        <div class="content">
+            <h2 style="font-family:'Bitter', serif; text-align:center; font-size: 50px; margin-top:25px">TEMPO</h2>
+                @csrf
+                <section>
+                <div class="content" style="float: left; width: 300px; padding: 70px; margin-left: 80px; margin-top: 90px; text-align: center; border-style: solid; color: #13136b; border-width: 5px; border-radius: 15px; font-family:'Didact Gothic', sans-serif;">
+                    <ul class="actions">
+                        <li>
+                            <a href="/approval" class="button primary" style="background-color: white; color: #13136b; font-size: 30px" >Approval</a>
+                        </li>
+                    </ul>
+                </div>
+                </section>
+                
+                <section>
+                <div class="content" style="float: right; width: 300px; padding: 70px; border-radius: 15px; margin-right: 120px; margin-top: 90px; text-align: center; color: #13136b; border-style: solid; border-width: 5px; font-family:'Didact Gothic', sans-serif;">
+                    <ul class="actions">
+                        <li>
+                            <a href="/open" class="button primary" style="background-color: white; color: #13136b; font-size: 30px">Open</a>
+                        </li>
+                    </ul>
+                </div>
+                </section>
+            
+        </div>
+    </div>
+</section>
+
+<!--
 <style>
     .center {
         height: 200px;
@@ -53,4 +84,5 @@
         </div>
     </div>
 </div>
+-->
 @endsection
