7e60b371637ed8966b912b1eaae275b84d639843
ldy-to-how <54564625+ldy-to-how@users.noreply.github.com>
2019-11-05 04:18:32 +0900

    asdf

--- resources/views/createFacility.blade.php
+++ resources/views/createFacility.blade.php
@@ -5,20 +5,20 @@
     <div class="inner">
         <header class="special">
 
-            <h1>Create Facility</h1>
+            <h1 style="font-size:60px">Create Facility</h1>
         </header>
     </div>
 
     <body>
-            <div class="inner">
-                <form action="/approval" method="POST" >
+            <div class="inner" style="display:flex; justify-content:center; align-items:center">
+                <form action="/approval" method="POST" style="width: 60%">
                     @csrf
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px;">
                         <label>Name</label> <input type="text" name="Name">
                     </div>
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px">
                         <label>Location</label>
                         <input type="radio" id="DormA" name="Location" value="DormA">
                         <label for="DormA">Dorm A</label>
@@ -26,7 +26,7 @@
                         <label for="DormB">Dorm B</label>
                     </div>
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px">
                         <label>Category</label>
                         <input type="radio" id="StudyRoom" name="Category" value="StudyRoom">
                         <label for="StudyRoom">Study Room</label>
@@ -36,7 +36,7 @@
                         <label for="MovieRoom">Movie Room</label>
                     </div>
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px">
                         <label>Type</label>
                         <input type="radio" id="Approval" name="Type" value="Approval">
                         <label for="Approval">Approval</label>
@@ -44,7 +44,7 @@
                         <label for="Open">Open</label>
                     </div>
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px">
                         <label>Status</label>
                         <input type="radio" id="Open" name="Status" value="Open">
                         <label for="Open">Open</label>
@@ -55,11 +55,11 @@
                     </div>
 
 
-                    <div class="form-input">
+                    <div class="form-input" style="margin-bottom: 10px">
                         <label>Capacity</label> <input type="number" name="Capacity">
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group" style="margin-bottom: 10px">
                         <label>Picture</label> <input type="file" name="Picture">
                     </div>
 
