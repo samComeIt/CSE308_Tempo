6343f8450f5bba924b4c1fc0728c570483319f94
samComeIt <40321847+samComeIt@users.noreply.github.com>
2019-11-17 13:56:39 +0900

    Update TimeslotController.php
    Created update() and delete() methods in the TimeslotController

--- app/Http/Controllers/TimeslotController.php
+++ app/Http/Controllers/TimeslotController.php
@@ -4,7 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class timeslotController extends Controller
+class TimeslotController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        //
+        \App\Timeslot::create([
+            'date' => $request->get('date'),
+            'start_time' => $request->get('start_time'),
+            'duration' => $request->get('duration'),
+            'facility_id' => $request->get('facility_id'),
+            ]);
+
+        return redirect('approval');
+    }
+    public function destroy($id)
+    {
+        $timeSelect = \App\Timeslot::find($id);
+        $timeSelect -> delete();
+        return redirect('/approval');
+    }
 }
