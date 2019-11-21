2544133cc596708762718e63f38a0538a751e196
samComeIt <40321847+samComeIt@users.noreply.github.com>
2019-11-21 23:36:42 +0900

    create Reservation model, controller, and table
    created MVC for Revervation class

---
+++ database/migrations/2019_11_21_021213_create_reservation_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->bigIncrements('reservation_id');
+            $table->string('type');
+            $table->integer('user_id');
+            $table->integer('timeslot_id');
+            $table->integer('facility_id');
+            $table->string('reservation_status');
+            $table->string('purpose');
+            $table->integer('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
