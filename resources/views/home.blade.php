812b89fb832d6c73226b1241549c4391a7b15309
khpark228 <kyuhee.park@stonybrook.edu>
2019-12-05 22:42:30 +0900

    home page, my page frontend change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,50 @@
+
+
 @extends('layouts.app')
-
 @section('content')
-
 <section id="main" class="wrapper">
     <div class="inner">
         <div class="content">
             <h2 style="font-family:'Bitter', serif; text-align:center; font-size: 60px">TEMPO</h2>
+            <h2 style="font-family:'Bitter', serif; text-align:center; font-size: 40px">Online Room Reservation</h2>
             @csrf
-            <section>
-                <div class="content"
-                     style="float: left; padding: 70px; margin-top: 90px; text-align: center; border-style: solid; color: #13136b; border-width: 5px; border-radius: 15px; font-family:'Didact Gothic', sans-serif;">
-                    <ul class="actions">
-                        <li>
-                            <a href="/approval" class="button primary"
-                               style="background-color: white; text-align: center; color: #13136b; font-size: 30px">Approval</a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <section>
-                <div class="content"
-                     style="float: right; padding: 70px; border-radius: 15px; margin-top: 90px; margin-right: 50px; text-align: center; color: #13136b; border-style: solid; border-width: 5px; font-family:'Didact Gothic', sans-serif;">
-                    <!-- padding-right: 90px; padding-left: 90px; -->
-                    <ul class="actions">
-                        <li>
-                            <a href="/open" class="button primary"
-                               style="background-color: white; text-align: center; display: inline; color: #13136b; font-size: 30px">Open</a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
+            <div class="division">
+                <ul class="actions" style="justify-content: center">
+                    <li>
+                        <a href="/approval" class="button primary"
+                        style="background-color: white; text-align: center; color: #13136b; font-size: 30px">Approval</a> 
+                    </li>
+                </ul>
+                <li>Library Study Rooms</li>
+            </div>
+            
+            <div class="division">
+                <ul class="actions" style="justify-content: center">
+                    <li>
+                        <a href="/approval" class="button primary"
+                        style="background-color: white; text-align: center; color: #13136b; font-size: 30px">Open</a>
+                    </li>
+                </ul>
+                <li>Study Rooms</li>
+            </div>
         </div>
     </div>
 </section>
+
+
+<style>
+    .division{
+        text-align: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 70px; 
+        margin-top: 90px; 
+        border-style: solid; 
+        color: #13136b; 
+        border-width: 5px; 
+        border-radius: 15px; 
+        font-family:'Didact Gothic', sans-serif;
+    }
+</style>
 @endsection
