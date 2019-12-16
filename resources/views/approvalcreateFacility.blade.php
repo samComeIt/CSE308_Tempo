fa0300717f8b04a3c06f8aca9ffc71a617f487c9
ldy-to-how <54564625+ldy-to-how@users.noreply.github.com>
2019-12-17 00:56:01 +0900

    Small change

--- resources/views/approvalcreateFacility.blade.php
+++ resources/views/approvalcreateFacility.blade.php
@@ -37,23 +37,6 @@
                 <label for="categoryOther">Other</label>
                 <textarea style="display:none" name="Category" rows="1" id="otherField" placeholder="Type in the new building" required></textarea>
             </div>
-            
-<!--
-            <div class="form-input" style="margin-bottom: 10px">
-                <label>Type</label>
-                <input type="radio" id="Approval" name="Type" value="Approval" checked readonly>
-                <label for="Approval">Approval</label>
-            </div>
-
-            <div class="form-input" style="margin-bottom: 10px">
-                <label>Status</label>
-                <input type="radio" id="Available" name="Status" value="Available" required>
-                <label for="Available">Available</label>
-                <input type="radio" id="OutofOrder" name="Status" value="OutofOrder">
-                <label for="OutofOrder">Out of Order</label>
-            </div>
--->
-
 
             <div class="form-input" style="margin-bottom: 10px">
                 <label>Capacity</label> <input type="number" min="1" max="1000" width="100%" name="Capacity" required>
@@ -63,12 +46,6 @@
                 <label>Description</label>
                 <textarea maxlength="300" name="Description" style="rows:7; cols: 40; height: 150px"></textarea>
             </div>
-
-<!--
-            <div class="form-group" style="margin-bottom: 10px">
-                <label>Picture</label> <input type="file" name="Picture">
-            </div>
--->
             
             <input type="hidden" id="Approval" name="Type" value="Approval">
             <input type="hidden" id="Available" name="Status" value="Available">
